3c55212ce71026d3c35c6a8df65028f077eca862
Joemx <jose.c.mondragon@gmail.com>
2020-10-20 18:19:36 -0500

    Updated insturctions
    instructions

--- PrimeCycle.java
+++ PrimeCycle.java
@@ -6,20 +6,46 @@ cycles explanation
 
 get a random number between 100 and 1000, write that amount of * in lines of 10
 
+###*#*#***
+#*#***#*#*
+**#*****#*
+
+
 EXTRA:
 each, prime number write a #
 */
 
 class PrimeCycle{
 
+    public static int fib(int meta){
+        int res1= 0;
+        int res2 =0;
+        int curr = 0;
+
+        for(int iter =0; iter <= meta ; iter++){
+            if( iter == 0){
+                res1 = 0;
+            }
+            if(iter ==1){
+                res2 = 1;
+            }
+
+            curr = res2+res1;
+            res2 = res1;
+            res1 = curr;
+        }
+        return curr;
+    }
+
     public static void main(String args[]){
         // how a while works
-
+        /*
         int res1= 0;
         int res2 =1;
         int curr = 0;
         int iter = 1;
         int meta = 6;
+
         while(iter <= meta){
             curr = res2+res1;
             res2 = res1;
@@ -27,14 +53,15 @@ class PrimeCycle{
             iter++;
         }
         System.out.println("Res " + curr);
-           
-    }
+        */
+        int res = fib(6);
+        System.out.println("Res 6:" + res);
+        res = fib(3);
+        System.out.println("Res 3:" + res);
+        res = fib(10);
+        System.out.println("Res 10:" + res);
 
-}
-
-
-
- /*
+        /*
         int i = 0;
         while(i < 10){  
             System.out.println("Hola");
@@ -50,3 +77,8 @@ class PrimeCycle{
         for( int j = 0; j < 10 ; j++){
             System.out.println("Hola");
         }*/
+
+        
+    }
+
+}
